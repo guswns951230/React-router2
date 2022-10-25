f68c2697d88c16043643aef40ca2c16ff8b8a167
Kanghj <rkdguswns0803@gmail.com>
2022-10-25 11:02:39 +0900

    Board list

---
+++ react-router/src/context/ContextComp.jsx
@@ -0,0 +1,6 @@
+import React, { createContext } from "react";
+
+// state를 담아서 전달
+export const StateContext = createContext(null);
+// set을 담아서 전달
+export const setContext = React.createContext(null);
--- react-router/src/page/BoardList.jsx
+++ react-router/src/page/BoardList.jsx
@@ -1,7 +1,24 @@
+import { useContext } from "react";
+import { Link, Outlet } from "react-router-dom";
+import { StateContext } from "../context/ContextComp";
+
 const BoardList = () => {
+  // 값을 가져올 때, value에 객체로 들어오지 않았다면 구조화할당을 할 필요 없다
+  const boardlist = useContext(StateContext);
   return (
     <div>
-      <h1>Board List</h1>
+      {boardlist &&
+        boardlist.map((item) => (
+          <Link to={"/boardlist/" + item.id} key={item.id}>
+            {item.id}
+          </Link>
+        ))}
+      {/* 
+        Outlet을 통해 props값을 전달할 수 없다
+        props값을 전달할 때는 component를 통해 직접 전달
+        -> App에 있는 component로 전달
+      */}
+      <Outlet />
     </div>
   );
 };
